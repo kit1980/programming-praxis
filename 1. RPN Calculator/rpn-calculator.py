fde50373517831787717a05cf5ce841d1b41c7ab
Sergey Dymchenko <kit1980@gmail.com>
2011-09-13 01:36:01 +0300

    Added Python solution for RPN Calculator.

---
+++ 1. RPN Calculator/rpn-calculator.py
@@ -0,0 +1,47 @@
+# Programming Praxis
+# RPN Calculator
+# http://programmingpraxis.com/2009/02/19/rpn-calculator/
+# Without any error handling.
+#
+# Sergey Dymchenko <kit1980@gmail.com>
+#
+# Language: Python 
+# Tested with Python 2.6.5
+# python rpn-calculator.py
+
+class RPN(object):
+    def __init__(self):
+        self.stack = []
+        self.operations = {
+            "+": lambda x, y: x + y,
+            "-": lambda x, y: x - y,
+            "*": lambda x, y: x * y,
+            "/": lambda x, y: x / y,
+            }
+
+    def process(self, expression_string):
+        tokens = expression_string.split()
+        for token in tokens:
+            try:
+                # this is a number, push to the stack
+                n = float(token)
+                self.stack.append(n)
+            except ValueError:
+                # an operation, do it and push the result to the stack
+                y = self.stack.pop() # note y is on the top of the stack
+                x = self.stack.pop()
+                result = self.operations[token](x, y)
+                self.stack.append(result)
+
+    def result(self):
+        return self.stack[-1]
+
+
+calc = RPN()
+while True:
+    try:
+        expression_string = raw_input()
+        calc.process(expression_string)
+        print calc.result()
+    except EOFError:
+        break
